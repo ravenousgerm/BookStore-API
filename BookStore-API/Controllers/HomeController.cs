d11cbf289724c9b6aa72f45f4f807e2552b98edc
Ryan McBride <rnmcbrd@hotmail.com>
2020-07-19 00:40:04 -0600

    Added DTOS and other data files.

---
+++ BookStore-API/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore_API.Conracts;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BookStore_API.Controllers
+{
+	/// <summary>
+	/// Test Hemo Controller anananananan
+	/// </summary>
+	[Route("api/[controller]")]
+	[ApiController]
+	public class HomeController : ControllerBase
+	{
+		private readonly ILoggerService _logger;
+
+		public HomeController(ILoggerService logger)
+		{
+			_logger = logger;
+		}
+
+		// GET: api/<HomeController>
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			_logger.LogInfo("Accessed Get");
+			return new string[] { "value1", "value2" };
+		}
+
+		// GET api/<HomeController>/5
+		[HttpGet("{id}")]
+		public string Get(int id)
+		{
+			_logger.LogInfo("Accessed Get and got a value");
+			return "value";
+		}
+
+
+		// POST api/<HomeController>
+		[HttpPost]
+		public void Post([FromBody] string value)
+		{
+			_logger.LogError(value);
+		}
+
+		// PUT api/<HomeController>/5
+		[HttpPut("{id}")]
+		public void Put(int id, [FromBody] string value)
+		{
+		}
+
+		// DELETE api/<HomeController>/5
+		[HttpDelete("{id}")]
+		public void Delete(int id)
+		{
+		}
+	}
+}
